62c9457aa1b6813060c021e81d205e18a443ae26
Al3xiel <gabriel280104casas@gmail.com>
2026-01-26 21:42:01 -0500

    feat(translations): add language switcher component for multilingual support

---
+++ src/app/shared/components/language-switcher/language-switcher.ts
@@ -0,0 +1,29 @@
+import {Component, inject} from '@angular/core';
+import {SelectButton} from 'primeng/selectbutton';
+import {FormsModule} from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [
+    SelectButton,
+    FormsModule
+  ],
+  templateUrl: './language-switcher.html',
+  styleUrl: './language-switcher.scss',
+})
+export class LanguageSwitcher {
+  private translate = inject(TranslateService);
+  stateOptions = [
+    { label: 'EN', value: 'en' },
+    { label: 'ES', value: 'es' }
+  ];
+  value: string = this.translate.getCurrentLang() || 'en';
+
+  changeLanguage(event: any) {
+    // event.value contiene 'en' o 'es'
+    if (event.value) {
+      this.translate.use(event.value);
+    }
+  }
+}
